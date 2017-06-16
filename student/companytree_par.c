c74220ac1894d97cb56c6bce98461e55706aaf63
Anibal <anibal.siguenza1@gmail.com>
2017-06-16 17:47:32 +0200

    successfull version

--- student/companytree_par.c
+++ student/companytree_par.c
@@ -1,8 +1,23 @@
 #include "companytree.h"
+#define T 8 //threshold
+
+void parallelTranverse(tree *node, int n){
+    if(node != NULL){
+        node->work_hours = compute_workHours(node->data);
+        top_work_hours[node->id] = node->work_hours;
+
+#pragma omp task firstprivate(n) final(n<T)
+        parallelTranverse(node->right, n-1);
+#pragma omp task firstprivate(n) final(n<T)
+        parallelTranverse(node->left, n-1);
+    }
+}
 
 void traverse(tree *node, int numThreads){
-
-//write your parallel solution here
-
+#pragma omp parallel num_threads(numThreads)
+    {
+#pragma omp single
+        parallelTranverse(node,numThreads);
+    }
 }
 
